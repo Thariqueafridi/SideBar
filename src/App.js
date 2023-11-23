516aa48516cdf1a557a318079f78c57c8271001b
Tharique Afridi <tharique@visiblestarsksa.com>
2023-11-23 14:35:41 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ButtonAppBar from './components/navbar/navbar';
+import TemporaryDrawer from './components/sidebar/sidebar';
+import Dashboard from './components/dashboard/dashboard';
+import Analytics from './components/analytics/analytics';
+import Surveys from './components/surveys/surveys';
+import Reports from './components/reports/reports';
+import Settings from './components/settings/settings';
+import Profile from './components/profile/profile';
+import User from './components/user/user';
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
+  sum(3,5)
+  function sum(a,b){
+    console.log(a,b)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className='App'>
+          <ButtonAppBar></ButtonAppBar>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard/>}></Route>
+            <Route path='/analytics' element={<Analytics/>}></Route>
+            <Route path='/surveys' element={<Surveys/>}></Route>
+            <Route path='/reports' element={<Reports/>}></Route>
+            <Route path='/settings' element={<Settings/>}></Route>
+            <Route path='/profile' element={<Profile/>}></Route>
+            <Route path='/user' element={<User/>}></Route>
+          </Routes>
+        </div>
+    </Router>
+    
   );
 }
 
